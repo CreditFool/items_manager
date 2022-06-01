a8d26e6731bf97142ed62a031d1454d137a26594
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 05:48:31 +0700

    add class ItemList n UserList at views item

--- item/views.py
+++ item/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import permissions, generics
+from rest_framework.response import Response
+
+from .models import Item
+from .serializers import ItemSerializer, UserSerializer
+
+
+class ItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
