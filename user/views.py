8514b2ce9a7ca1e74fbd48cd3e9ec8ee8e6070bc
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 02:07:39 +0700

    adding DeleteUserToken function

--- user/views.py
+++ user/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
-from rest_framework import permissions
+from rest_framework import permissions, generics, status
 from rest_framework.views import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 
+from .serializers import TokenSerializer
+
 
 class GetUserToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
+        # authentication_classes = (BasicAuthentication)
         serializer = self.serializer_class(
             data=request.data,
             context={'request': request}
@@ -22,3 +25,16 @@ class GetUserToken(ObtainAuthToken):
             'email': user.email,
             'username': user.username,
         })
+
+
+class DeleteUserToken(generics.DestroyAPIView):
+    queryset = Token.objects.all()
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            token = Token.objects.get(key=request.auth)
+            token.delete()
+            return Response({"detail": "Token Deleted Successfully"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"detail": "Token Not Provided"}, status=status.HTTP_401_UNAUTHORIZED)
