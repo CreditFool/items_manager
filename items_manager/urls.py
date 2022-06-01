1b18efd20551680373afbdb1bf55e85a8f3dd8a2
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 00:38:31 +0700

    add url login

--- items_manager/urls.py
+++ items_manager/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user.views import GetUserToken
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', GetUserToken.as_view()),
 ]
