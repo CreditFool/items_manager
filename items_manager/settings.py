c2a397bc4baca2f49bf3ecb18e3b048be1760432
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 00:02:56 +0700

    edit settings

--- items_manager/settings.py
+++ items_manager/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'items_manager',
+    'user',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
