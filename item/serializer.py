59476384b1d6bd9652ac1f5753fcb72498a06912
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 05:36:35 +0700

    update serializer item

--- item/serializer.py
+++ item/serializer.py
@@ -5,12 +5,27 @@ from .models import Item
 
 
 class UserSerializer(serializers.ModelSerializer):
+    items = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Item.objects.all()
+    )
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'items', 'owner')
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('id', 'name', 'description', 'price', 'owner')
+    
+    def create(self, validated_data):
+        return Item.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        for field in fields(Item):
+            if field.name in validated_data:
+                setattr(instance, field.name, validated_data[field.name])
+        instance.save()
+        return instance
