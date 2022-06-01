e634b6d5ef7eff0d1090cc0034e40c4e6de27b5c
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 05:30:39 +0700

    add serializer on app item

---
+++ item/serializer.py
@@ -0,0 +1,16 @@
+from dataclasses import fields
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Item
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('id', 'name', 'description', 'price', 'owner')
