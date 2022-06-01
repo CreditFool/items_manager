cb053136566bcd6baba6a26132329191e196385a
creditFool <bichlasulbulqiah@gmail.com>
2022-06-02 00:24:44 +0700

    add GetAuthToken

--- user/views.py
+++ user/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import permissions
+from rest_framework.views import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+
+
+class GetUserToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'email': user.email,
+            'username': user.username,
+        })
